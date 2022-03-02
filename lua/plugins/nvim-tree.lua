c5483dddf0b7c3207805b8d635ac64a5e74fa1a9
chien.le <chien.le@shopee.com>
2022-03-02 09:29:52 +0700

    update

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,7 +1,6 @@
 local g = vim.g
 
 g.nvim_tree_auto_ignore_ft = { "startify", "dashboard" } -- empty by default, don't auto open tree on specific filetypes.
-g.nvim_tree_quit_on_open = 0 -- 0 by default, closes the tree when you open a file
 g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
 g.nvim_tree_git_hl = 0 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
 g.nvim_tree_highlight_opened_files = 0 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
@@ -9,7 +8,6 @@ g.nvim_tree_root_folder_modifier = ":~" -- This is the default. See :help filena
 g.nvim_tree_width_allow_resize = 1 -- will resize the tree when opening a file
 g.nvim_tree_add_trailing = 0 -- 0 by default, append a trailing slash to folder names
 g.nvim_tree_group_empty = 0 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
-g.nvim_tree_disable_window_picker = 0 -- 0 by default, will disable the window picker.
 g.nvim_tree_special_files = { "README.md", "Makefile", "MAKEFILE" } -- List of filenames that gets highlighted with NvimTreeSpecialFile
 g.nvim_tree_show_icons = { git = 0, folders = 1, files = 1 }
 
@@ -17,22 +15,7 @@ g.nvim_tree_icon_padding = " " -- one space by default, used for rendering the s
 g.nvim_tree_symlink_arrow = " ➛ " --  defaults to ' ➛ '. used as a separator between symlinks' source and target.
 g.nvim_tree_respect_buf_cwd = 0 -- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
 g.nvim_tree_refresh_wait = 1000 -- 1000 by default, control how often the tree can be refreshed, 1000 means the tree can be refresh once per 1000ms.
-g.nvim_tree_window_picker_exclude = {
-    filetype = {
-        "notify",
-        "packer",
-        "qf",
-    },
-    buftype = {
-        "terminal",
-    },
-}
--- Dictionary of buffer option names mapped to a list of option values that
--- indicates to the window picker that the buffer's window should not be
--- selectable.
 
--- default will show icon by default if no icon is provided
--- default shows no icon by default
 g.nvim_tree_icons = {
     default = "",
     symlink = "",
@@ -67,12 +50,15 @@ require("nvim-tree").setup({
     disable_netrw = true,
     hijack_netrw = true,
     open_on_setup = false,
+    ignore_buffer_on_setup = false,
     ignore_ft_on_setup = {},
     auto_close = false,
+    auto_reload_on_write = true,
     open_on_tab = false,
     hijack_cursor = false,
     update_cwd = false,
-    update_to_buf_dir = {
+    hijack_unnamed_buffer_when_opening = false,
+    hijack_directories = {
         enable = true,
         auto_open = true,
     },
@@ -86,10 +72,7 @@ require("nvim-tree").setup({
         },
     },
     update_focused_file = {
-        -- enables the feature
         enable = true,
-        -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-        -- only relevant when `update_focused_file.enable` is true
         update_cwd = false,
         ignore_list = {},
     },
@@ -111,10 +94,35 @@ require("nvim-tree").setup({
         height = 30,
         hide_root_folder = false,
         side = "left",
-        auto_resize = false,
+        preserve_window_proportions = false,
         mappings = {
             custom_only = false,
             list = {},
+        },
+        number = false,
+        relativenumber = false,
+        signcolumn = "yes",
+    },
+    trash = {
+        cmd = "trash",
+        require_confirm = true,
+    },
+    actions = {
+        change_dir = {
+            enable = true,
+            global = false,
+        },
+        open_file = {
+            quit_on_open = false,
+            resize_window = false,
+            window_picker = {
+                enable = true,
+                chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+                exclude = {
+                    filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+                    buftype = { "nofile", "terminal", "help" },
+                },
+            },
         },
     },
 })

5a44abfd7bde34a99a6ec190a0de8f605dfa65e0
chien.le <chien.le@shopee.com>
2022-07-04 14:02:10 +0800

    update

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -2,7 +2,17 @@
 require("orgmode").setup_ts_grammar()
 
 require("orgmode").setup({
-    org_agenda_files = { "~/Dropbox/org/*", "~/my-orgs/**/*" },
-    org_default_notes_file = "~/Dropbox/org/refile.org",
+    org_agenda_files = "~/Dropbox/org/*",
+    org_default_notes_file = "~/Dropbox/org/inbox.org",
     org_hide_leading_stars = true,
+    org_ellipsis = "",
+    org_blank_before_new_entry = {
+        heading = false,
+        plain_list_item = false,
+    },
+    mappings = {
+        org = {
+            -- org_toggle_checkbox = "<leader>x",
+        },
+    },
 })

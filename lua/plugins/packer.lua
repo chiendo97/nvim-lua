d75de804fc083e6799cf5798b0ef5c7f6d7bfe72
chien.le <chien.le@shopee.com>
2022-09-11 09:31:04 +0800

    update

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -149,9 +149,18 @@ return require("packer").startup(function(use)
 
     -- vim-tmux-navigation
     use({
-        "christoomey/vim-tmux-navigator",
+        "alexghergh/nvim-tmux-navigation",
         config = function()
-            vim.g.tmux_navigator_disable_when_zoomed = 1
+            local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+            nvim_tmux_nav.setup({
+                disable_when_zoomed = true, -- defaults to false
+            })
+
+            vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+            vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+            vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+            vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
         end,
     })
 

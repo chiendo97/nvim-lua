4bd843d416ad77567c4dc1d9337c8794abb820a0
Chien Le <chienlt@homic.vn>
2024-07-08 10:43:52 +0700

    fix: gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,11 +2,11 @@ local gs = require("gitsigns")
 
 gs.setup({
     signs = {
-        add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        -- add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        -- change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        -- delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        -- topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        -- changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
     },
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
     numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -69,15 +69,15 @@ gs.setup({
     },
     attach_to_untracked = true,
     current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-    current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-        delay = 1000,
-        ignore_whitespace = false,
-    },
-    current_line_blame_formatter_opts = {
-        relative_time = false,
-    },
+    -- current_line_blame_opts = {
+    --     virt_text = true,
+    --     virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+    --     delay = 1000,
+    --     ignore_whitespace = false,
+    -- },
+    -- current_line_blame_formatter_opts = {
+    --     relative_time = false,
+    -- },
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil, -- Use default
@@ -90,7 +90,7 @@ gs.setup({
         row = 0,
         col = 1,
     },
-    yadm = {
-        enable = false,
-    },
+    -- yadm = {
+    --     enable = false,
+    -- },
 })

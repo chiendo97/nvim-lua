0134438d8a10cc109ce9f2b4079d2b2fd2baff1c
chiendo97 <chiendo97@gmail.com>
2024-10-17 10:58:47 +0700

    update

--- after/ftplugin/org.lua
+++ after/ftplugin/org.lua
@@ -1,3 +1,5 @@
 -- vim.opt.spell = true
 -- vim.opt.spelllang = { "en_us" }
 vim.o.wrap = true -- wrap long lines
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"

6a904c70e82cc88cc233f32d918b12bd3a947972
chiendo97 <chiendo97@gmail.com>
2024-10-20 19:42:20 +0700

    feat: blink.cmp

--- lua/lsp/attach.lua
+++ lua/lsp/attach.lua
@@ -40,6 +40,7 @@ M.on_attach = function(client, bufnr)
     end
 end
 
-M.capabilities = require("cmp_nvim_lsp").default_capabilities()
+-- M.capabilities = require("cmp_nvim_lsp").default_capabilities()
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
 
 return M

7aefab5e76ad955aa64cad87fac0bd069a4c73c7
chiendo97 <chiendo97@gmail.com>
2024-12-06 15:52:03 +0700

    update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -41,6 +41,8 @@ require("nvim-treesitter.configs").setup({
     incremental_selection = {
         enable = true,
         keymaps = {
+            init_selection = "",
+            scope_incremental = "",
             node_incremental = "v",
             node_decremental = "V",
         },

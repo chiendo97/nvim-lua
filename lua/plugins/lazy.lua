09a99f8a77222375dca2e5c57e1163c5de076eeb
chiendo97 <chiendo97@gmail.com>
2024-12-19 09:58:10 +0700

    feat: replace blink with cmp

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -38,53 +38,53 @@ require("lazy").setup({
         end,
     },
 
-    {
-        "hrsh7th/nvim-cmp",
-        -- "yioneko/nvim-cmp",
-        -- branch = "perf-up",
-        -- "iguanacucumber/magazine.nvim",
-        lazy = true,
-        config = function()
-            require("plugins.nvim-cmp")
-        end,
-    },
-    { "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" },
-    { "hrsh7th/cmp-buffer", event = "InsertEnter" },
-    -- { "hrsh7th/cmp-nvim-lsp-signature-help", event = "InsertEnter" },
-    { "hrsh7th/cmp-path", event = "InsertEnter" },
-    {
-        "zbirenbaum/copilot-cmp",
-        -- disable if node is not executable
-        enabled = vim.fn.executable("node") == 1,
-        event = "InsertEnter",
-        config = function()
-            require("copilot_cmp").setup()
-        end,
-        dependencies = {
-            "zbirenbaum/copilot.lua",
-            cmd = "Copilot",
-            build = ":Copilot auth",
-            config = function()
-                require("copilot").setup({
-                    panel = {
-                        enabled = false,
-                    },
-                    suggestion = {
-                        enabled = false,
-                    },
-                    filetypes = {
-                        yaml = true,
-                        markdown = true,
-                        help = true,
-                        gitcommit = true,
-                        svn = false,
-                        cvs = false,
-                        ["."] = false,
-                    },
-                })
-            end,
-        },
-    },
+    -- {
+    --     "hrsh7th/nvim-cmp",
+    --     -- "yioneko/nvim-cmp",
+    --     -- branch = "perf-up",
+    --     -- "iguanacucumber/magazine.nvim",
+    --     lazy = true,
+    --     config = function()
+    --         require("plugins.nvim-cmp")
+    --     end,
+    -- },
+    -- { "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" },
+    -- { "hrsh7th/cmp-buffer", event = "InsertEnter" },
+    -- -- { "hrsh7th/cmp-nvim-lsp-signature-help", event = "InsertEnter" },
+    -- { "hrsh7th/cmp-path", event = "InsertEnter" },
+    -- {
+    --     "zbirenbaum/copilot-cmp",
+    --     -- disable if node is not executable
+    --     enabled = vim.fn.executable("node") == 1,
+    --     event = "InsertEnter",
+    --     config = function()
+    --         require("copilot_cmp").setup()
+    --     end,
+    --     dependencies = {
+    --         "zbirenbaum/copilot.lua",
+    --         cmd = "Copilot",
+    --         build = ":Copilot auth",
+    --         config = function()
+    --             require("copilot").setup({
+    --                 panel = {
+    --                     enabled = false,
+    --                 },
+    --                 suggestion = {
+    --                     enabled = false,
+    --                 },
+    --                 filetypes = {
+    --                     yaml = true,
+    --                     markdown = true,
+    --                     help = true,
+    --                     gitcommit = true,
+    --                     svn = false,
+    --                     cvs = false,
+    --                     ["."] = false,
+    --                 },
+    --             })
+    --         end,
+    --     },
+    -- },
 
     {
         "kylechui/nvim-surround",
@@ -481,6 +481,128 @@ require("lazy").setup({
             })
             require("fzf-lua").register_ui_select()
         end,
+    },
+    {
+        "saghen/blink.cmp",
+        -- optional: provides snippets for the snippet source
+        dependencies = {
+            {
+                "rafamadriz/friendly-snippets",
+                {
+                    "giuxtaposition/blink-cmp-copilot",
+                    enabled = vim.fn.executable("node") == 1,
+                    dependencies = {
+                        "zbirenbaum/copilot.lua",
+                        cmd = "Copilot",
+                        build = ":Copilot auth",
+                        config = function()
+                            require("copilot").setup({
+                                panel = {
+                                    enabled = false,
+                                },
+                                suggestion = {
+                                    enabled = false,
+                                },
+                                filetypes = {
+                                    yaml = true,
+                                    markdown = true,
+                                    help = true,
+                                    gitcommit = true,
+                                    svn = false,
+                                    cvs = false,
+                                    ["."] = false,
+                                },
+                            })
+                        end,
+                    },
+                },
+            },
+        },
+
+        -- use a release tag to download pre-built binaries
+        version = "v0.*",
+        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+        -- build = 'cargo build --release',
+        -- If you use nix, you can build from source using latest nightly rust with:
+        -- build = 'nix run .#build-plugin',
+
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            keymap = {
+                ["<CR>"] = { "accept" },
+                ["<C-n>"] = { "select_next" },
+                ["<C-p>"] = { "select_prev" },
+                ["<C-l>"] = { "show", "hide" },
+                ["<C-_>"] = { "cancel" },
+                ["<C-Space>"] = { "show_documentation", "hide_documentation" },
+                ["<C-f>"] = { "scroll_documentation_down" },
+                ["<C-u>"] = { "scroll_documentation_up" },
+                ["<Tab>"] = { "snippet_forward", "fallback" },
+                ["<S-Tab>"] = { "snippet_backward", "fallback" },
+            },
+            completion = {
+                list = {
+                    selection = "preselect",
+                },
+                accept = {
+                    auto_brackets = {
+                        enabled = false,
+                        semantic_token_resolution = {
+                            enabled = false,
+                        },
+                    },
+                },
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 0,
+                    treesitter_highlighting = true,
+                    window = {
+                        border = "rounded",
+                    },
+                },
+                ghost_text = {
+                    enabled = true,
+                },
+                menu = {
+                    border = "rounded",
+                    draw = {
+                        columns = {
+                            { "label", "label_description", gap = 1 },
+                            { "kind" },
+                        },
+                    },
+                },
+            },
+            sources = {
+                providers = {
+                    copilot = {
+                        name = "copilot",
+                        module = "blink-cmp-copilot",
+                        score_offset = 100,
+                        async = true,
+                        transform_items = function(_, items)
+                            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+                            local kind_idx = #CompletionItemKind + 1
+                            CompletionItemKind[kind_idx] = "Copilot"
+                            for _, item in ipairs(items) do
+                                item.kind = kind_idx
+                            end
+                            return items
+                        end,
+                    },
+                },
+                completion = {
+                    enabled_providers = { "lsp", "path", "snippets", "buffer", "copilot" },
+                },
+            },
+            appearance = {
+                use_nvim_cmp_as_default = false,
+            },
+        },
+        -- allows extending the providers array elsewhere in your config
+        -- without having to redefine it
+        opts_extend = { "sources.default" },
     },
 })
 

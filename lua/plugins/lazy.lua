f9fbb23a6801d064e540fcd6f77a97778673ac42
chiendo97 <chiendo97@gmail.com>
2025-01-15 12:23:31 +0700

    feat: check node for copilot

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -510,23 +510,23 @@ require("lazy").setup({
             },
             sources = {
                 providers = {
-                    copilot = {
-                        name = "copilot",
-                        module = "blink-cmp-copilot",
-                        score_offset = 100,
-                        async = true,
-                        transform_items = function(_, items)
-                            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-                            local kind_idx = #CompletionItemKind + 1
-                            CompletionItemKind[kind_idx] = "Copilot"
-                            for _, item in ipairs(items) do
-                                item.kind = kind_idx
-                            end
-                            return items
-                        end,
-                    },
+                    copilot = vim.fn.executable("node") == 1
+                            and {
+                                name = "copilot",
+                                module = "blink-cmp-copilot",
+                                score_offset = 100,
+                                async = true,
+                            }
+                        or nil,
                 },
-                default = { "lsp", "path", "snippets", "buffer", "copilot" },
+                default = function(_)
+                    local default = { "lsp", "path", "snippets", "buffer", "copilot" }
+                    if vim.fn.executable("node") == 1 then
+                        table.insert(default, "copilot")
+                    end
+
+                    return default
+                end,
             },
             appearance = {
                 use_nvim_cmp_as_default = false,

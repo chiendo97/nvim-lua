fcddfff4c2d76410630934aaed5c6a5ba5d6841f
chiendo97 <chiendo97@gmail.com>
2025-02-06 07:25:23 +0700

    feat: markdown fold treesitter

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,9 @@
+-- vim.opt.spell = true
+
+vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt_local.foldlevel = 10
+vim.opt_local.foldmethod = "expr"
+vim.opt_local.foldenable = true
+vim.opt_local.foldminlines = 1
+
+-- vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"

32719dfbf8c68d81af64785a99fbe3264747b43b
chiendo97 <chiendo97@gmail.com>
2025-05-24 19:36:02 +0700

    update

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -7,3 +7,24 @@ vim.opt_local.foldenable = true
 vim.opt_local.foldminlines = 1
 
 -- vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
+
+-- c: auto-wrap comments using textwidth
+-- r: auto-insert the current comment leader after hitting <Enter>
+-- o: auto-insert the current comment leader after hitting 'o' or 'O'
+-- q: allow formatting comments with 'gq'
+-- n: recognize numbered lists
+-- 1: don't break a line after a one-letter word
+-- j: remove comment leader when it makes sense
+-- this gets overwritten by ftplugins (:verb set fo)
+-- we use autocmd to remove 'o' in '/lua/autocmd.lua'
+-- borrowed from tjdevries
+vim.opt.formatoptions = vim.opt.formatoptions
+    - "a" -- Auto formatting is BAD.
+    - "t" -- Don't auto format my code. I got linters for that.
+    + "c" -- In general, I like it when comments respect textwidth
+    + "q" -- Allow formatting comments w/ gq
+    - "o" -- O and o, don't continue comments
+    + "r" -- But do continue when pressing enter.
+    + "n" -- Indent past the formatlistpat, not underneath it.
+    + "j" -- Auto-remove comments if possible.
+    - "2" -- I'm not in gradeschool anymore

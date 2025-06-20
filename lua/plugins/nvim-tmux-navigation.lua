d7b0107eaa8c9f44ac425a6a28746fecf276ab18
chiendo97 <chiendo97@gmail.com>
2025-06-20 11:12:05 +0700

    feat: navigate keymaps in terminal

--- lua/plugins/nvim-tmux-navigation.lua
+++ lua/plugins/nvim-tmux-navigation.lua
@@ -13,6 +13,11 @@ return {
             vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
             vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
             vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+
+            vim.keymap.set("t", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+            vim.keymap.set("t", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+            vim.keymap.set("t", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+            vim.keymap.set("t", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
         end,
     },
 }

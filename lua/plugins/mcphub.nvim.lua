69df4fe539ff13d2725064eeeff2c3c3d4238949
chiendo97 <chiendo97@gmail.com>
2025-09-04 11:39:23 +0700

    fix: typo

--- lua/plugins/mcphub.nvim.lua
+++ lua/plugins/mcphub.nvim.lua
@@ -3,7 +3,7 @@ local is_mcphub_available = vim.fn.executable("mcp-hub") == 1
 return {
     {
         "ravitemer/mcphub.nvim",
-        enable = is_mcphub_available,
+        enabled = is_mcphub_available,
         opts = {
             auto_approve = true, -- Auto approve mcp tool calls
         },

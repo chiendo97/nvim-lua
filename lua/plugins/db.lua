31e13f94e872296dba1ce4913ce51cde03bc92bc
chiendo97 <chiendo97@gmail.com>
2025-10-24 06:49:55 +0700

    update

--- lua/plugins/db.lua
+++ lua/plugins/db.lua
@@ -29,41 +29,5 @@ return {
             -- OPTIONAL: Bookmark symbol (default: "🌟 ")
             bookmark_symbol = "🌟 ",
         },
-        init = function()
-            -- OPTIONAL: Change to your liking or drop completely
-            -- The plugin does not provide default key mappings, only commands
-            local map = vim.keymap.set
-            map("n", "<leader>nto", ":NeovimTips<CR>", { desc = "Neovim tips", noremap = true, silent = true })
-            map(
-                "n",
-                "<leader>nte",
-                ":NeovimTipsEdit<CR>",
-                { desc = "Edit your Neovim tips", noremap = true, silent = true }
-            )
-            map(
-                "n",
-                "<leader>nta",
-                ":NeovimTipsAdd<CR>",
-                { desc = "Add your Neovim tip", noremap = true, silent = true }
-            )
-            map(
-                "n",
-                "<leader>nth",
-                ":help neovim-tips<CR>",
-                { desc = "Neovim tips help", noremap = true, silent = true }
-            )
-            map(
-                "n",
-                "<leader>ntr",
-                ":NeovimTipsRandom<CR>",
-                { desc = "Show random tip", noremap = true, silent = true }
-            )
-            map(
-                "n",
-                "<leader>ntp",
-                ":NeovimTipsPdf<CR>",
-                { desc = "Open Neovim tips PDF", noremap = true, silent = true }
-            )
-        end,
     },
 }

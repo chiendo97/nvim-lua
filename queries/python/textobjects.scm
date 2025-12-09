46e45d185c0e13b2efa6dd74421ddb30e6cb9b12
chiendo97 <chiendo97@gmail.com>
2025-12-09 15:11:13 +0700

    update

--- queries/python/textobjects.scm
+++ queries/python/textobjects.scm
@@ -1,9 +1,8 @@
 ((function_definition
     body: (block)? @function.inner)) @function.outer
 
-((decorated_definition)?
-  (class_definition
-    body: (block)? @class.inner)) @class.outer
+(class_definition
+  body: (block)? @class.inner) @class.outer
 
 (while_statement
   body: (block)? @loop.inner) @loop.outer
